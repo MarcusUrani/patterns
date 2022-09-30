f2ef53fbcd6697106bc0906980389dbc89510e76
AlunoTGN <marcus.urani20@gmail.com>
2022-09-29 21:48:22 -0300

    Iniciando o projeto

---
+++ patterns.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+  int i;
+  char first_text[1001];
+  scanf("%s", first_text);
+  int first_size = strlen(first_text);
+  char second_text[first_size];
+  scanf("%s", second_text);
+  for(i=0; i < first_size; i++) {
+    if(second_text[i] == first_text[i]) {
+      
+    }
+  }
+  return 0;
+}
