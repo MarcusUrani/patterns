7f79d4853d4cba2a02bb0891f7170eb34edb4eb8
Marcus <marcus.urani20@gmail.com>
2022-10-04 14:07:21 -0300

    Continuando o projeto

--- patterns.c
+++ patterns.c
@@ -4,17 +4,17 @@
 int main(void)
 {
   int i;
-  char first_text[1001];
-  scanf("%s", first_text);
+  char first_text[1002];
+  char second_text[102];
+  fgets(first_text, 1002, stdin);
+  fgets(second_text, 102, stdin);
   int first_size = strlen(first_text);
-  char second_text[first_size];
-  scanf("%s", second_text);
   int second_size = strlen(second_text);
   for (i = 0; i < first_size; i++)
   {
     if (second_text[i] == first_text[i])
     {
-      printf("Essa e a letra:\n");
+      printf("Essa eh a letra:\n");
     }
   }
   return 0;
